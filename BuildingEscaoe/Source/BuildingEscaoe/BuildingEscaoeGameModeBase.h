ca226c5b24ffc8ed34965b800ab4cab2b8dacfdc
andrzejkantor9 <anderzejn@gmail.com>
2019-02-21 02:58:56 +0100

    Initial commit
    Former-commit-id: 2b64c3156d2a04fdbc3b5a8204cce81e077343c2

---
+++ BuildingEscaoe/Source/BuildingEscaoe/BuildingEscaoeGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BuildingEscaoeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUILDINGESCAOE_API ABuildingEscaoeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
