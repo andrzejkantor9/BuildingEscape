a595eef7a3aab7b61649ffe94cd458a89c66b007
andrzejkantor9 <anderzejn@gmail.com>
2019-03-08 13:40:40 +0100

    drawing camera tracing debug line
    Former-commit-id: 8974dabdb733e64a0bed89e190787647db834f1b

--- BuildingEscaoe/Source/BuildingEscaoe/Grabber.cpp
+++ BuildingEscaoe/Source/BuildingEscaoe/Grabber.cpp
@@ -1,6 +1,11 @@
 // copyright Andrzej Kantor 2019
 
 #include "Grabber.h"
+#include "Gameframework/Actor.h"
+#include "Engine.h"
+#include "DrawDebugHelpers.h"
+
+#define OUT
 
 // Sets default values for this component's properties
 UGrabber::UGrabber()
@@ -29,6 +34,22 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	// Get the player veiwport this tick
+	FVector PlayerViewPointLocation;
+	FRotator PlayerViewPointRotation;
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+		OUT PlayerViewPointLocation, OUT PlayerViewPointRotation);
+
+	//log out to test
+	//UE_LOG(LogTemp, Warning, TEXT("Camera Location: %s Camera Rotation: %s"), 
+	//	*PlayerViewPointLocation.ToString() , *PlayerViewPointRotation.ToString());
+
+	//draw a red trace to visualise
+	FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * DebugLineReach; 
+	DrawDebugLine(GetWorld(), PlayerViewPointLocation, LineTraceEnd, FColor(255, 0, 0), false, 0.f, 0.f, 10.f);
+
+	//ray-cast out to reacch distancez
+
+	//see what we hit
 }
 
--- BuildingEscaoe/Source/BuildingEscaoe/Grabber.h
+++ BuildingEscaoe/Source/BuildingEscaoe/Grabber.h
@@ -24,5 +24,6 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
+private:
+	const float DebugLineReach = 100.f;;
 };
